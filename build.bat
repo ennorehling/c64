d4b2c14f16c3899e16841dbb785f1f2db11ceab8
Enno Rehling <enno.rehling@gmail.com>
2013-08-05 15:23:02 -0700

    seizure-inducing screen flickers.

---
+++ build.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+C:\C64\dasm-2.20.11\bin\dasm.exe %1.asm -o%1.prg
+IF ERRORLEVEL 1 GOTO fail
+C:\C64\WinVICE-2.2-x64\x64.exe %1.prg
+:fail
---
+++ flash.asm
@@ -0,0 +1,8 @@
+	processor 6502
+	
+	org $1000
+	
+loop:	inc $d020
+	inc $d021
+	jmp loop
+
