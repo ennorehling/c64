3bfe85e2fd24845c491faa782789098d9fb11d03
Enno Rehling <enno.rehling@gmail.com>
2013-08-05 22:11:58 -0700

    header that lets me run a program with RUN.

--- text.asm
+++ text.asm
@@ -1,7 +1,11 @@
 ; draw some text
 	processor 6502
 
-	org $0810 ; sys2064
+	org $0801 ; sys2061
+sysline:
+	.byte $0b,$08,$01,$00,$9e,$32,$30,$36,$31,$00,$00,$00
+
+	org $080d ; sys2061
 
 	jsr $e544 ; clear screen
 
