1c058e3f89ba4c0616770ed21e46f0157e240a2a
Enno Rehling <enno.rehling@gmail.com>
2013-08-05 22:21:52 -0700

    magic header that starts the program on RUN.

--- flash.asm
+++ flash.asm
@@ -1,8 +1,12 @@
 ; dasm source
 	processor 6502
 	
-	org $1000
-	
+	org $0801 ; sys2061
+sysline:
+	.byte $0b,$08,$01,$00,$9e,$32,$30,$36,$31,$00,$00,$00
+
+	org $080d ; sys2061
+
 loop:	inc $d020
 	inc $d021
 	jmp loop
